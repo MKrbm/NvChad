b562408f6aed49f9e93518559a893578425e4dd1
MKrbm <mukeisuke0709@gmail.com>
2023-08-27 16:37:27 +0000

    test

---
+++ test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string str = "Hello World!";
+    std::cout << str << std::endl;
+    std::cout << str3 << std::endl;
+    return 0;
+}
